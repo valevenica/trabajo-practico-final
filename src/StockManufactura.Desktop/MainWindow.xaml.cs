36f63a2ac683a0d9074c72932e20b0f0b533ca3a
actualizacion infraestructura <vale.venica@gmail.com>
2026-08-04 12:42:09 -0300

    fix(login): ventana centrada y tamaño inicial compacto

--- src/StockManufactura.Desktop/MainWindow.xaml.cs
+++ src/StockManufactura.Desktop/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using StockManufactura.Desktop.Infrastructure;
+using StockManufactura.Desktop.ViewModels;
 
 namespace StockManufactura.Desktop;
 
@@ -9,11 +11,72 @@ namespace StockManufactura.Desktop;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private const double LoginWidth = 860;
+    private const double LoginHeight = 620;
+    private const double LoginMinWidth = 760;
+    private const double LoginMinHeight = 520;
+    private const double ShellWidth = 1240;
+    private const double ShellHeight = 760;
+    private const double ShellMinWidth = 900;
+    private const double ShellMinHeight = 560;
+
+    private MainWindowViewModel? _viewModel;
+
     public MainWindow()
     {
         InitializeComponent();
         TryLoadHeaderLogo();
         TryLoadWindowIcon();
+        DataContextChanged += OnDataContextChanged;
+        Loaded += (_, _) => ApplyWindowLayoutForCurrentState();
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (_viewModel is not null)
+        {
+            _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
+        }
+
+        _viewModel = e.NewValue as MainWindowViewModel;
+        if (_viewModel is not null)
+        {
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        ApplyWindowLayoutForCurrentState();
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MainWindowViewModel.IsShellVisible))
+        {
+            ApplyWindowLayoutForCurrentState();
+        }
+    }
+
+    private void ApplyWindowLayoutForCurrentState()
+    {
+        var shellVisible = _viewModel?.IsShellVisible == true;
+
+        if (shellVisible)
+        {
+            MinWidth = ShellMinWidth;
+            MinHeight = ShellMinHeight;
+            Width = ShellWidth;
+            Height = ShellHeight;
+        }
+        else
+        {
+            MinWidth = LoginMinWidth;
+            MinHeight = LoginMinHeight;
+            Width = LoginWidth;
+            Height = LoginHeight;
+        }
+
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        WindowState = WindowState.Normal;
+        UpdateLayout();
     }
 
     private void TryLoadHeaderLogo()
